37a3fc7d49cf27382d8449dbd72f647f90f2580e
eshika <eshikasaxena@hotmail.com>
2018-02-25 17:08:58 -0800

    Added histogram and csv for features
    Histogram shows RGB values
    
    CSV for cell features

--- HemaCam Code/features.py
+++ HemaCam Code/features.py
@@ -1,35 +1,36 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Feb 23 01:38:56 2018
+Created on Sun Feb 25 15:26:42 2018
 
 @author: eshikasaxena
 
-color histograms for each cell
+write features to csv
 """
 import numpy as np
 import cv2
-from HemaCamSegmentation import img_load, thresh
+from HemaCamSegmentation import img_load, thresh, extractFeatures
 from scipy import ndimage
 from skimage.feature import peak_local_max
 from skimage.morphology import watershed
-from matplotlib import pyplot as plt
+import csv
 
 global rootpath, imgnum
 rootpath = "C:\\Users\\eshikasaxena\\Desktop\\HemaCam Project\\Code\\"
-imgnum = 0
+imgnum = 100
 
-def calcHist(roi):
-    roi_copy = roi.copy()
-    cellgray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-    cellthresh = thresh(roi, cellgray)
-    cellmask = cellthresh
-    color = ('b','g','r')
-    for i,col in enumerate(color):
-        histr = cv2.calcHist([roi_copy],[i],cellmask,[256],[0,256])
-        plt.plot(histr,color = col)
-        plt.xlim([0,256])
-    plt.show()
-    
+def write_csv(data, filename):
+    with open(rootpath + filename + '.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(data)
+#        for x in data:
+#            writer.writerow(x)
+
+def append_csv(data, filename):
+    with open(rootpath + filename + '.csv', 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow(data)
+#        for x in data:
+#            writer.writerow(x)
 
 def watershed_segmentation(img, threshold, gray):
     contours = []
@@ -39,6 +40,7 @@ def watershed_segmentation(img, threshold, gray):
     	labels=threshold)
     markers = ndimage.label(localMax, structure=np.ones((3, 3)))[0]
     labels = watershed(-D, markers, mask=threshold)
+    write_csv(['Cell Number', 'Perimeter', 'Area', 'Circularity', 'Major Axis', 'Minor Axis', 'Ratio'], 'data_{}'.format(imgnum))
     count = 1
     for label in np.unique(labels):
         if label == 0:
@@ -46,26 +48,21 @@ def watershed_segmentation(img, threshold, gray):
         mask = np.zeros(gray.shape, dtype="uint8")
         mask[labels == label] = 255         
         cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-        cv2.drawContours(clean, cnts, -1, (255,0,0), 3)
         c = max(cnts, key=cv2.contourArea)
         if c.shape[0] >= 5:
             x,y,w,h = cv2.boundingRect(c)
+            features = extractFeatures(c)
+            features = [count] + features
+            append_csv(features, 'data_{}'.format(imgnum))
             roi = clean[y:y+h, x:x+w]
-            calcHist(roi)
-#            cv2.imshow("p", cellthresh)
-#            cv2.waitKey(0)
-#            cv2.destroyAllWindows()
             contours.append(c)    
-        count += 1
+            count += 1
+            
+        
+#    print num, count
 
 if __name__ == "__main__":
-#    img, gray = img_load(imgnum)
-#    clean = img.copy()
-#    imgthresh = thresh(img, gray)
-#    watershed_segmentation(clean, imgthresh, gray)
-#    cv2.waitKey(0)
-#    cv2.destroyAllWindows()
-    img = cv2.imread('c40.png')
-    calcHist(img)
-
-
+    img, gray = img_load(imgnum)
+    clean = img.copy()
+    imgthresh = thresh(img, gray)
+    watershed_segmentation(clean, imgthresh, gray)
